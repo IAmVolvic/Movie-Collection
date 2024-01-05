274b72226f40553b07d91a50e16f61a33d97663a
IAmVolvic <71038869+IAmVolvic@users.noreply.github.com>
2024-01-05 12:46:27 +0100

    Worked

--- src/GUI/MainController.java
+++ src/GUI/MainController.java
@@ -6,12 +6,19 @@ import io.github.palexdev.materialfx.controls.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.MFXTableView;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
 import io.github.palexdev.materialfx.filter.StringFilter;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.Comparator;
 import java.util.ResourceBundle;
+
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
 
 public class MainController implements Initializable {
     @FXML
@@ -45,5 +52,18 @@ public class MainController implements Initializable {
         ratingColumn.setRowCellFactory(movie -> new MFXTableRowCell<Movie, Object>(Movie::getRating));
         lastViewed.setRowCellFactory(movie -> new MFXTableRowCell<Movie, Object>(Movie::getLastViewed));
         moviesTableView.getTableColumns().addAll(idColumn,titleColumn,ratingColumn,lastViewed);
+    }
+
+    public void testRunVideo(ActionEvent actionEvent) {
+        System.out.println("Running Video");
+
+        String videoPath = "resources/assets/testVideo.mp4";
+
+        try {
+            File videoFile = new File(videoPath);
+            Desktop.getDesktop().open(videoFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
