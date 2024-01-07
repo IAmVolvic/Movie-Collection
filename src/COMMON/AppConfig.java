292a38ebb967b6e3bd681f94be7a008417a52113
IAmVolvic <71038869+IAmVolvic@users.noreply.github.com>
2024-01-07 13:12:03 +0100

    Moved Files

--- src/COMMON/AppConfig.java
+++ src/COMMON/AppConfig.java
@@ -1,4 +1,4 @@
-package APP_SETTINGS;
+package COMMON;
 
 public class AppConfig {
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import APP_SETTINGS.AppConfig;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
 
 public class Main extends Application {
 
